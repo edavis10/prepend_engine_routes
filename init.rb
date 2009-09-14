8a04d7160e0d67edd7711ecd870602ccf851eeb9
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-14 16:34:20 -0700

    Pluginized initial hack for Rails 2.3.4

---
+++ init.rb
@@ -0,0 +1,48 @@
+module PrependEngineRoutes
+  module Configuration
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        attr_accessor :prepended_route_configuration_files
+      end
+    end
+    
+    module InstanceMethods
+      def prepended_route_configuration_files
+        @prepended_route_configuration_files || []
+      end
+
+      def add_prepended_route_configuration_file(file)
+        @prepended_route_configuration_files ||= []
+        @prepended_route_configuration_files << file
+      end
+    end
+  end
+
+  module Initializer
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        alias_method_chain :initialize_routing, :prepend_engine
+      end
+    end
+    
+    module InstanceMethods
+      def prepend_configuration_file(file)
+        self.prepended_routes ||= []
+        self.prepended_routes << file
+      end
+      
+      def initialize_routing_with_prepend_engine
+        configuration.prepended_route_configuration_files.each do |prepended_route_config|
+          ActionController::Routing::Routes.add_configuration_file(prepended_route_config)
+        end
+        initialize_routing_without_prepend_engine
+      end
+    end      
+  end
+end
+
+Rails::Configuration.send :include, PrependEngineRoutes::Configuration
+Rails::Initializer.send :include, PrependEngineRoutes::Initializer
+
